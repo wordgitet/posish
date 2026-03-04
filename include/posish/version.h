ac18bfe7f837d8845c75a1b1526d117300f3dbec
wordgitet <wordatet@linuxmail.org>
2026-03-04 23:15:07 +0200

    version: derive POSISH_VERSION from configure package version

--- include/posish/version.h
+++ include/posish/version.h
@@ -1,6 +1,14 @@
 #ifndef POSISH_VERSION_H
 #define POSISH_VERSION_H
 
-#define POSISH_VERSION "0.1.0-skeleton"
+/*
+ * Single source of truth: configure.ac AC_INIT version -> PACKAGE_VERSION.
+ * Build system injects config.h, which defines PACKAGE_VERSION.
+ */
+#ifdef PACKAGE_VERSION
+#define POSISH_VERSION PACKAGE_VERSION
+#else
+#define POSISH_VERSION "unknown"
+#endif
 
 #endif
